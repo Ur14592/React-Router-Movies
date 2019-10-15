d5a11b78d0dc908a1c09eb6c682826928f2ee244
Alex Vega <alexvega880@Alexs-MacBook-Air.local>
2019-10-15 14:21:05 -0700

    Starting

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-
+import MovieList from './Movies/MovieList';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+import ReactDom from "react-dom";
 import SavedList from './Movies/SavedList';
 
 const App = () => {
@@ -13,8 +15,11 @@ const App = () => {
     <div>
       <SavedList list={savedList} />
       <div>Replace this Div with your Routes</div>
+      <Route exact path="/" component={MovieList} />
+
     </div>
-  );
+      
+  );    
 };
 
 export default App;
